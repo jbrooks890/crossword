ed93813b7a8617430d524255596ecfec05afb72b
ArcanBoi <61268478+ArcanBoi@users.noreply.github.com>
2022-10-22 14:31:29 -0400

    minors

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -2,11 +2,14 @@
 import { ReactComponent as XWORD_LOGO } from "../../assets/icons/xword-logo-2.svg";
 import "../../styles/Header.css";
 import Nav from "./Nav";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const navigate = useNavigate();
+
   return (
     <header>
-      <h2 id="site-logo" className="flex">
+      <h2 id="site-logo" className="flex" onClick={() => navigate("/")}>
         <XWORD_LOGO />
       </h2>
       <Nav />
